ae5da6f822e589ee161ca4ab21446a66e6f75b80
rexvel <teteriv22@gmail.com>
2020-10-09 15:45:21 +0300

    lab1 fix

--- 1-Basics/Lab1.2.py
+++ 1-Basics/Lab1.2.py
@@ -1,23 +1,18 @@
 def long_word(sen):
     return max(filter(lambda x: x.isalpha(), sen.split()), key=len)
-test = "Turmoil has engulfed the Galactic Republic  The taxationChargoggagoggmanchauggagoggchaubunagungamaugg Republic "
+test = "Turmoil has engulfed the Galacticgarlic Republic  The test test test test Republic "
 
 mock = "Chargoggagoggmanchauggagoggchaubunagungamaugg"
 
 def most_freq(str):
-    counts = dict()
-    words = str.split()
-
-
-    for word in words:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
-
-    counts_x = sorted(counts.items(), key=lambda kv: kv[1])
-    #print(counts_x)
-    return counts_x[-2]
+    l = str.split()
+    d = {}
+    for i in l:
+        if i not in d.keys():
+            d[i] = 0
+        d[i] = d[i]+1
+    print(d)
+    return d
 res = long_word(test)
     
 res2 = most_freq(test)
